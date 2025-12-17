bc04dbca58868598c353aee6ddf5cc5d0d711a76
Pamindu Karunadasa <khaag8689@allfreemail.net>
2025-12-17 22:11:18 +0530

    2nd commit
    2nd commit

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
-// config.js
-// This file checks if we are on Vercel or Local
 const CONFIG = {
-    SUPABASE_URL: window.SUPABASE_URL || 'YOUR_LOCAL_SUPABASE_URL',
-    SUPABASE_KEY: window.SUPABASE_KEY || 'YOUR_LOCAL_ANON_KEY'
+    // 1. Go to Supabase Dashboard -> Settings (Cog icon) -> API
+    // 2. Copy "Project URL" and paste it below inside the quotes
+    SUPABASE_URL: 'https://xemzausoygrlwoiuzqcn.supabase.co',
+
+    // 3. Copy "anon public" Key and paste it below inside the quotes
+    // (Do NOT use the service_role key!)
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlbXphdXNveWdybHdvaXV6cWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5ODU4NjAsImV4cCI6MjA4MTU2MTg2MH0.UIqbvBXjURu8iFLmPi7_sAzZxZDXX5Ud8zhTzcpJENg'
 };
